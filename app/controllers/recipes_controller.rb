fd7cfa964109d66fde4823ab2ecf028e6cdc83d7
Alan Hong <xiupan@gmail.com>
2017-07-31 14:57:45 -0500

    Basic CRUD working for Recipes.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,6 +17,27 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    redirect_to root_path
+  end
+
   private
   def recipe_params
     params.require(:recipe).permit(:name)
